04cc446a88cccd689140d0dbdad0c8b11b37d89d
czr01 <cz4rk01@gmail.com>
2023-05-03 21:06:56 +0200

    improve exception handling

---
+++ exceptions.py
@@ -0,0 +1,8 @@
+class LSysConfigSyntaxError(Exception):
+    pass
+
+class JSONFileRequiredError(ValueError):
+    pass
+
+class NegativeIterationsError(ValueError):
+    pass
--- main.py
+++ main.py
@@ -4,35 +4,46 @@ import os
 import json
 
 from lsystem import LSystem
+from exceptions import *
 
-def main():
-    
+def main():  
     filename, iterations = get_input()
     
     lsystem = parse(filename)
 
     lsys_string = lsystem.process(5)
     print(lsys_string)
-    #
+
     turtle = Turtle()
     lsystem.draw(lsys_string, turtle)
-
     
 def get_input():
-    filename = input("Name of file containing l-system: ")
-    iterations = int(input("Number of iterations: "))
-    return filename, iterations
+    try:
+        filename = input("Name of file containing l-system: ")
+        if not os.path.exists(filename): 
+            raise FileNotFoundError(f"No such file: {filename}")
+        if not filename.endswith(".json"): 
+            raise JSONFileRequiredError("Input file must be '.json'")
+
+        iterations = int(input("Number of iterations: "))
+        if iterations < 0:
+            raise NegativeIterationsError("Number of iterations can't be negative")
+        
+        return filename, iterations
+    
+    except (FileNotFoundError, JSONFileRequiredError, NegativeIterationsError, ValueError) as e:
+        print(e)
+        sys.exit(1)
 
 def parse(filename):
     try:
         with open(filename,'r') as f:
             data = json.load(f)
             return LSystem(data["axiom"], data["rules"], data["translations"], int(data["angle"]), int(data["length"]))
-
-    except FileNotFoundError as fnfe:
-        print(fnfe)
-    except Exception as e:
-        print(e)
+        
+    except json.decoder.JSONDecodeError as jde:
+        print(f"Invalid JSON. {jde}")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
