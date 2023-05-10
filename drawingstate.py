ba23c1475110dc9df2c383290530498685ca9a1c
czr01 <cz4rk01@gmail.com>
2023-05-10 23:53:01 +0200

    reworked file structure

--- drawingstate.py
+++
@@ -1,8 +0,0 @@
-class DrawingState:
-    def __init__(self, x, y, angle):
-        self.x = x
-        self.y = y
-        self.angle = angle
-
-    def __repr__(self):
-        return f"DrawingState({self.x}, {self.y}, {self.angle}, {self.color})"
